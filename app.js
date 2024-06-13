d0981d42a76172b99c1d7b47b1a337de8a8a5ab1
Liedson Franco <126687225+LiedsonFranco@users.noreply.github.com>
2024-06-13 11:33:58 +0100

    challenge completed

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 let theme_toggler = document.getElementById('toggler')
-theme_toggler.addEventListener('change', () => {
+let currentTheme = document.getElementById('currentTheme')
+document.addEventListener('DOMContentLoaded', () => {
+    theme_toggler.value = localStorage.getItem('themeMode')
+    changeTheme()
+})
+theme_toggler.addEventListener('change', changeTheme)
+function changeTheme(){
     if(theme_toggler.value == 1){
         document.body.classList.add('dark')
+        localStorage.setItem('themeMode', 1)
+        currentTheme.innerHTML = 'Dark Mode'
     }
     else{
         document.body.classList.remove('dark')
+        localStorage.setItem('themeMode', 0)
+        currentTheme.innerHTML = 'Light Mode'
+
     }
-})
+}
